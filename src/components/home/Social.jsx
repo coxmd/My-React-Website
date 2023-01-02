e7eb2aadafa426b0c1b0e14320fc6407ba9900d6
coxmd <coxmusyoki@gmail.com>
2023-01-02 23:09:20 +0300

    Completed the home section of the webpage

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/cox-musyoki-62ab96185/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/coxmd"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/coxmd"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-whatsapp"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social;
